97f31c1ae053888309d855244948402fc6c10d5a
Amandio Orepolo <Dinarte.orepolo@gmail.com>
2025-02-07 22:48:51 +0000

    Style the search bar and the results

--- index.js
+++ index.js
@@ -13,41 +13,44 @@ const resultsBox = document.querySelector('.results');
 const searchResult = "Your search results here.";
 
 
+let wines = [];
+
+
 fetch('wines.json')
-.then(response => response.json())
-.then(data => {
-    const wines = data;
+    .then(response => response.json())
+    .then(data => {
+        wines = data;
+    });
 
-    function searchWines() {
-        const searchIt = document.getElementById("search");
-        const resultsOn = document.getElementById("results");
-        resultsOn.innerHTML = '';
+function searchWines() {
+    const searchIt = document.getElementById("search");
+    const resultsOn = document.getElementById("results");
+    resultsOn.innerHTML = '';
 
-        const searchValue = searchIt.value.toLowerCase();
+    const searchValue = searchIt.value.toLowerCase();
 
-        const findWines = wines.filter(wine => {
-            return( 
-                wine.name.toLowerCase().includes(searchIt) ||
-                wine.country.toLowerCase().includes(searchValue)
-            );
-        });
+    const findWines = wines.filter(wine => {
+        return (
+            wine.name.toLowerCase().includes(searchValue) ||
+            wine.country.toLowerCase().includes(searchValue)
+        );
+    });
 
-        if (findWines.length > 0 ) {
-            findWines.forEach(wine => {
-                const card = document.createElement("div");
-                card.classList.add("card-box");
-                card.innerHTML = `
+    if (findWines.length > 0) {
+        findWines.forEach(wine => {
+            const card = document.createElement("div");
+            card.classList.add("card-box");
+            card.innerHTML = `
                 <div class="card-wrapper">
                 <h3 class="card-title">${wine.name} (${wine.year})</h3>
                     <p class="card-text">${wine.grape} - ${wine.country}</p>
                     <p class="card-text">${wine.description}</p>
                 </div>`;
-                resultsOn.appendChild(card);
-            });
-        }else{
-            resultsOn.innerHTML = '<p>No wines found.</p>';  
-        }
+            resultsOn.appendChild(card);
+        });
+    } else {
+        resultsOn.innerHTML = '<p>No wines found.</p>';
     }
+}
 
-    window.searchWines = searchWines;
-});
+window.searchWines = searchWines;
