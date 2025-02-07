2f3ffec89f4633cd4c8c34740413d3805f3a4609
Amandio Orepolo <Dinarte.orepolo@gmail.com>
2025-02-07 01:50:56 +0000

    Make a Navbar Responsive to for smaller screens

---
+++ index.js
@@ -0,0 +1,9 @@
+// Menu toogle and Nav-list 
+const menuToogle = document.querySelector(".menu-toggle");
+const navList = document.querySelector('.nav-list');
+
+// On click event listener Toogle menu
+menuToogle.addEventListener('click', () => {
+    navList.classList.toggle('appear');
+
+});
